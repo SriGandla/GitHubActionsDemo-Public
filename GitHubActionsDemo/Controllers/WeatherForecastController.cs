bbfeff693cbb16c9b602f5f97fb0eb8e8f78573b
SriGandla <33186647+SriGandla@users.noreply.github.com>
2024-12-19 13:08:23 -0600

    Update WeatherForecastController.cs

--- GitHubActionsDemo/Controllers/WeatherForecastController.cs
+++ GitHubActionsDemo/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace GitHubActionsDemo.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Swelterin1g", "Scorching"
     };
 
         private readonly ILogger<WeatherForecastController> _logger;
